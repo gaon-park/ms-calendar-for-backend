bd89f4d0dbec28a10197b17380240e48796849a6
gaon-park <do.judo1224@gmail.com>
2023-01-02 01:49:47 +0900

    note: todo

--- src/main/kotlin/com/maple/heroforbackend/entity/TEmailToken.kt
+++ src/main/kotlin/com/maple/heroforbackend/entity/TEmailToken.kt
@@ -3,12 +3,12 @@ package com.maple.heroforbackend.entity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import org.hibernate.annotations.GenericGenerator
 import java.time.LocalDateTime
 
+// todo redis나 아무튼 자동으로 유효기간 지나면 사라지는 데베로 변경
 @Entity
 @Table(name = "t_email_token")
 data class TEmailToken(
