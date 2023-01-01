68f1750e7ebfd54ec89b9ffec08f92fe699e0290
gaon-park <do.judo1224@gmail.com>
2023-01-01 16:16:59 +0900

    hello world

---
+++ src/main/kotlin/com/maple/heroforbackend/api/IndexController.kt
@@ -0,0 +1,12 @@
+package com.maple.heroforbackend.api
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class IndexController {
+    @GetMapping
+    fun index(): String {
+        return "index"
+    }
+}
