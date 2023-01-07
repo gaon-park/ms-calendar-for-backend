2edde348b9fc325848305a35e03d3da3b6209605
gaon-park <do.judo1224@gmail.com>
2023-01-07 14:17:32 +0900

    fix: initialization

--- src/test/kotlin/com/maple/heroforbackend/ScheduleServiceTest.kt
+++ src/test/kotlin/com/maple/heroforbackend/ScheduleServiceTest.kt
@@ -19,6 +19,7 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.slot
 import java.time.LocalDateTime
+import java.util.*
 
 class ScheduleServiceTest : BehaviorSpec() {
     init {
@@ -36,10 +37,10 @@ class ScheduleServiceTest : BehaviorSpec() {
 
         val tScheduleSlot: CapturingSlot<TSchedule> = slot()
         val tScheduleMemberSlot: CapturingSlot<List<TScheduleMember>> = slot()
-        val tEmailSlot: CapturingSlot<List<String>> = slot()
+        val emailSlot: CapturingSlot<List<String>> = slot()
 
         val owner = TUser(
-            id = 1,
+            id = 0,
             email = "do.judo1224@gmail.com",
             nickName = "do.judo1224@gmail.com",
             pass = "",
@@ -50,7 +51,7 @@ class ScheduleServiceTest : BehaviorSpec() {
         val members = listOf(
             owner,
             TUser(
-                id = 2,
+                id = 1,
                 email = "helloproud25@naver.com",
                 nickName = "helloproud25@naver.com",
                 pass = "",
@@ -59,7 +60,7 @@ class ScheduleServiceTest : BehaviorSpec() {
                 updatedAt = LocalDateTime.now()
             ),
             TUser(
-                id = 3,
+                id = 2,
                 email = "helloproud68@naver.com",
                 nickName = "helloproud68@naver.com",
                 pass = "",
@@ -68,7 +69,7 @@ class ScheduleServiceTest : BehaviorSpec() {
                 updatedAt = LocalDateTime.now()
             )
         )
-        val request = ScheduleAddRequest(
+        val addRequest = ScheduleAddRequest(
             title = "first",
             start = LocalDateTime.now(),
             end = null,
@@ -82,7 +83,7 @@ class ScheduleServiceTest : BehaviorSpec() {
         afterContainer {
             tScheduleSlot.clear()
             tScheduleMemberSlot.clear()
-            tEmailSlot.clear()
+            emailSlot.clear()
         }
 
         every {
@@ -105,9 +106,9 @@ class ScheduleServiceTest : BehaviorSpec() {
                 emptyList()
             }
             When("개인 스케줄 입력을 요청하면") {
-                service.insert(owner, request)
+                service.insert(owner, addRequest)
                 Then("1명의 스케줄 입력이 이루어진다") {
-                    tScheduleSlot.captured.title shouldBe request.title
+                    tScheduleSlot.captured.title shouldBe addRequest.title
                     tScheduleMemberSlot.captured.size shouldBe 1
                     tScheduleMemberSlot.captured[0].user.id shouldBe owner.id
                 }
@@ -115,7 +116,7 @@ class ScheduleServiceTest : BehaviorSpec() {
 
             When("파티 스케줄 입력을 요청하면") {
                 val exception = shouldThrow<BaseException> {
-                    service.insert(owner, request.copy(members = listOf(members[1].email)))
+                    service.insert(owner, addRequest.copy(members = listOf(members[1].email)))
                 }
                 Then("USER_NOT_FOUND 예외가 발생한다") {
                     tScheduleMemberSlot.isCaptured shouldBe false
@@ -127,31 +128,31 @@ class ScheduleServiceTest : BehaviorSpec() {
         // method: insert
         Given("owner 를 제외한 멤버가 있는 상황에서") {
             every {
-                tUserRepository.findByEmailIn(capture(tEmailSlot))
+                tUserRepository.findByEmailIn(capture(emailSlot))
             } answers {
-                members.filter { tEmailSlot.captured.contains(it.email) }
+                members.filter { emailSlot.captured.contains(it.email) }
             }
 
             When("파티 스케줄을 입력하면(owner not in members)") {
-                val curReq = request.copy(members = listOf(members[1].email, members[2].email))
+                val curReq = addRequest.copy(members = listOf(members[1].email, members[2].email))
                 service.insert(owner, curReq)
                 Then("스케줄과 request.members 데이터가 모두 입력된다") {
                     tScheduleSlot.isCaptured shouldBe true
                     tScheduleSlot.captured.title shouldBe curReq.title
-                    tEmailSlot.isCaptured shouldBe true
+                    emailSlot.isCaptured shouldBe true
                     tScheduleMemberSlot.isCaptured shouldBe true
                     tScheduleMemberSlot.captured.size shouldBe curReq.members.size + 1
                 }
             }
 
             When("파티 스케줄을 입력하면(owner in members)") {
-                val curReq = request.copy(members = listOf(members[0].email, members[1].email, members[2].email))
+                val curReq = addRequest.copy(members = listOf(members[0].email, members[1].email, members[2].email))
                 service.insert(owner, curReq)
                 Then("스케줄과 request.members 데이터가 모두 입력된다") {
                     tScheduleSlot.isCaptured shouldBe true
                     tScheduleSlot.captured.title shouldBe curReq.title
-                    tEmailSlot.isCaptured shouldBe true
-                    tEmailSlot.captured.size shouldBe curReq.members.size - 1
+                    emailSlot.isCaptured shouldBe true
+                    emailSlot.captured.size shouldBe curReq.members.size - 1
                     tScheduleMemberSlot.isCaptured shouldBe true
                     tScheduleMemberSlot.captured.size shouldBe curReq.members.size
                 }
