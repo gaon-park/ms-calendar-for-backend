b3b97956775271ec98536bd185eae432f79ed3fe
gaon-park <do.judo1224@gmail.com>
2023-01-25 17:44:57 +0900

    fix: for dev env

--- src/main/kotlin/com/maple/herocalendarforbackend/config/SecurityConfig.kt
+++ src/main/kotlin/com/maple/herocalendarforbackend/config/SecurityConfig.kt
@@ -33,10 +33,11 @@ class SecurityConfig(
             .and()
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authorizeHttpRequests() // 요청에 대한 권한 체크
-            .requestMatchers("/api/user", "/api/user/**").hasRole("USER")
-            .anyRequest().permitAll() // 그 외 나머지 요청은 누구나 접근 가능
+            // todo 본방 개시 전, 삭제
+//            .and()
+//            .authorizeHttpRequests() // 요청에 대한 권한 체크
+//            .requestMatchers("/api/user", "/api/user/**").hasRole("USER")
+//            .anyRequest().permitAll() // 그 외 나머지 요청은 누구나 접근 가능
             .and()
             .addFilterBefore(
                 JwtAuthenticationFilter(jwtAuthService),
