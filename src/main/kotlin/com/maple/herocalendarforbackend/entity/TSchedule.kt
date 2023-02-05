74028ee9e7442a891ace519b94d7520622b2033c
gaon-park <do.judo1224@gmail.com>
2023-02-06 00:48:10 +0900

    test: detekt

--- src/main/kotlin/com/maple/herocalendarforbackend/entity/TSchedule.kt
+++ src/main/kotlin/com/maple/herocalendarforbackend/entity/TSchedule.kt
@@ -12,6 +12,7 @@ import java.time.LocalDateTime
 
 @Entity
 @Table(name = "t_schedule")
+@Suppress("MagicNumber")
 data class TSchedule(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
