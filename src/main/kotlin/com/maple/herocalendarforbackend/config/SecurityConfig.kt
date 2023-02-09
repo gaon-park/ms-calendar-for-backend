d22e0308534e0a028ae99aafc84ab2cadf53f6d9
gaon-park <do.judo1224@gmail.com>
2023-02-09 12:00:54 +0900

    fix: config

--- src/main/kotlin/com/maple/herocalendarforbackend/config/SecurityConfig.kt
+++ src/main/kotlin/com/maple/herocalendarforbackend/config/SecurityConfig.kt
@@ -62,7 +62,7 @@ class SecurityConfig(
     fun corsConfigurationSource(): CorsConfigurationSource =
         CorsConfiguration().apply {
             allowedHeaders = listOf("*")
-            allowedOrigins = listOf("http://127.0.0.1:3000", "http://ms-hero.kr")
+            allowedOrigins = listOf("http://127.0.0.1:3000", "http://ms-hero.kr", "http://34.64.205.58")
             allowedMethods = listOf("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
             allowCredentials = true
         }.let { config ->
